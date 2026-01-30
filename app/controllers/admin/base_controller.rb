11725f8d4fc9f633c2547f45e69e1a1d281d3bfc
hinatahoriba <c0a2212903@edu.teu.ac.jp>
2026-01-31 01:36:54 +0900

    add: デザインの調整

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,4 +1,5 @@
 class Admin::BaseController < ApplicationController
+  layout "admin"
   http_basic_authenticate_with name: Rails.application.credentials.dig(:admin, :name) || ENV["ADMIN_USER"] || "admin",
                                password: Rails.application.credentials.dig(:admin, :password) || ENV["ADMIN_PASSWORD"] || "admin"
 end
